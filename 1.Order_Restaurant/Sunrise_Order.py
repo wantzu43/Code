40918b2a07ad6f9717a92cb9e0da91c1fdf5dc00
wantzu43 <131442361+wantzu43@users.noreply.github.com>
2023-05-17 13:24:09 +0800

    0517

--- 1.Order_Restaurant/Sunrise_Order.py
+++ 1.Order_Restaurant/Sunrise_Order.py
@@ -1,30 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 11 10:48:51 2023
+Created on Wed May 17 12:22:26 2023
 
 @author: Wantzu
 """
 
-import time,datetime
-from bs4 import BeautifulSoup
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-url = 'https://www.feastogether.com.tw/booking/Inparadise'
+url = 'https://www.feastogether.com.tw/booking/Sunrise'
 
 driver = webdriver.Chrome('chromedriver')
 driver.get(url)
-driver.implicitly_wait(3)
+driver.implicitly_wait(5)
 driver.maximize_window()
 
-'''
-for i in range(1000):
-    time.sleep(3)
-    driver.refresh()
-    if time.now().isoformat() == '12:15:00':
-        break
-'''
 
 #關閉公告頁面
 X = driver.find_element(By.CLASS_NAME, 'css-ywgpzb')
@@ -35,27 +27,27 @@ login = driver.find_element(By.CLASS_NAME, 'fStTgo')
 login.click()
 
 phone = driver.find_element(By.NAME, 'phoneNumber')
-phone.send_keys('0975226621')
+phone.send_keys('09XXXXX')#這邊需輸入電話
 
 pwd = driver.find_element(By.NAME, 'password')
-pwd.send_keys('ji394su3')
+pwd.send_keys('XXXXX')#這邊需輸入密碼
 
 OK = driver.find_element(By.CLASS_NAME, 'login')
 OK.click()
-time.sleep(1)
+time.sleep(3)
 
 #下拉選單畫布
 svg = driver.find_elements(By.CLASS_NAME, 'css-1q69m9j')
 svg[0].click() #店別
 
 #點選店別
-area = driver.find_elements(By.CLASS_NAME, 'frLmcK')
-area[1].click() #0是微風店、1是新莊店
+area = driver.find_element(By.CLASS_NAME, 'frLmcK')
+area.click() 
 
 
 svg[1].click() #成員
-num = driver.find_elements(By.CLASS_NAME, 'dsDilA')
-for i in range(8):
+num = driver.find_elements(By.CLASS_NAME, 'hjjfwZ')
+for i in range(2): #這邊代表2位成人，如需8人就改成range(8)
     num[0].click()
 
 
@@ -63,41 +55,30 @@ timesvg = driver.find_elements(By.CLASS_NAME, 'css-33zf0e')
 timesvg[0].click()
 time.sleep(1)
 
-time1 = driver.find_elements(By.CLASS_NAME, 'css-50viod')
-time1[1].click() #午餐、下午餐、晚餐
+time1 = driver.find_elements(By.CLASS_NAME, 'css-1tla7oo')
+time1[2].click() #午餐、下午餐、晚餐
 time.sleep(1)
 
-datesvg = driver.find_elements(By.CLASS_NAME, 'css-srhau5')
+datesvg = driver.find_elements(By.CLASS_NAME, 'css-2gu9x4')
 datesvg[2].click()
 
-date1 = driver.find_elements(By.CLASS_NAME, 'react-datepicker__day--0' + str(12))
+date1 = driver.find_elements(By.CLASS_NAME, 'react-datepicker__day--0' + str(15))#這邊是日期，輸入要搶的日期，今天5/16，0點要搶6/17號的，這邊就改成17
 date1[1].click()
- 
-search = driver.find_element(By.CLASS_NAME, 'searchBooking')
+
+search = driver.find_element(By.CLASS_NAME, 'ijfYSV')
 search.click()
-print(search.text)
-time.sleep(1)
-
-isee = driver.find_elements(By.CLASS_NAME, 'QlVA-D')
-isee[1].click()
-
+time.sleep(2)
 
 btndiv = driver.find_element(By.CLASS_NAME, 'bLsuFy')
 btns = btndiv.find_elements(By.TAG_NAME, 'button')
 btns[0].click()
-
-#驗證碼區塊
-code = driver.find_element(By.NAME, 'captchValue')
-
+time.sleep(10)
 
 send = btns[len(btns) - 1]
 print(send.text)
 send.click()
-time.sleep(1)
+time.sleep(2)
 
-iseediv = driver.find_element(By.CLASS_NAME, 'hQEXJm')
-iseediv.find_element(By.TAG_NAME, 'button').click()
-time.sleep(1)
 
 html = driver.find_element(By.TAG_NAME, 'html')
 html.send_keys(Keys.END)
